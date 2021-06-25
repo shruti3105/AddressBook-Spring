b84b816b64bc6ed39aefe8c975cdd9cef8b65274
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-25 21:59:33 +0530

    lobok library

--- src/main/java/com/addressbook/DTO/ResponseDTO.java
+++ src/main/java/com/addressbook/DTO/ResponseDTO.java
@@ -1,6 +1,8 @@
 package com.addressbook.DTO;
 
-public class ResponseDTO {
+import lombok.Data;
+
+public @Data class ResponseDTO {
 	
 	private String message;
 	private Object data;
@@ -9,25 +11,4 @@ public class ResponseDTO {
 		this.message = message;
 		this.data = data;
 	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
-
-	@Override
-	public String toString() {
-		return "ResponseDTO [message=" + message + ", data=" + data + "]";
-	}	
 }
