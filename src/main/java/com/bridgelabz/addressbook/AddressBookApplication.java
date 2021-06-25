dd96d0c5010ae66a77fd1031f1ead23928a6e9d4
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-25 22:06:04 +0530

    use lombok library for logging

--- src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
@@ -2,12 +2,19 @@ package com.bridgelabz.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("AddressBook Application Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+		log.info("AddressBook DB User is {} Environment",
+    			context.getEnvironment().getProperty("spring.datasource.username"));
 	}
-
 }
