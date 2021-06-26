989cc8714826e28ceb9de9bd4efbbf3e1b34ca4a
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-26 12:01:40 +0530

    provide user friendly error response

--- src/main/java/com/addressbook/service/AddressBookService.java
+++ src/main/java/com/addressbook/service/AddressBookService.java
@@ -1,28 +1,35 @@
 package com.addressbook.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.addressbook.exceptions.AddressBookException;
 import com.addressbook.DTO.AddressBookDTO;
 import com.addressbook.model.AddressBookData;
 import com.addressbook.repository.AddressBookRepository;
 
 @Service
 public class AddressBookService implements IAddressBookService {
-	
+
 	@Autowired
 	private AddressBookRepository addressBookRepository;
 
 	@Override
-	public List<AddressBookData> getAddressBookContactData() {		
+	public List<AddressBookData> getAddressBookContactData() {
 		return (List<AddressBookData>) addressBookRepository.findAll();
 	}
 
 	@Override
 	public AddressBookData getAddressBookContactDataById(int id) {
-		return addressBookRepository.findById(id).get();
+		try {
+			AddressBookData contactData = addressBookRepository.findById(id).get();
+			return contactData;
+		} catch (NoSuchElementException exception) {
+			throw new AddressBookException("Address Book Contact Not Found");
+		}
 	}
 
 	@Override
@@ -35,7 +42,7 @@ public class AddressBookService implements IAddressBookService {
 	@Override
 	public AddressBookData updateAddressBookContactData(int id, AddressBookDTO addressBookDTO) {
 		AddressBookData contactData = this.getAddressBookContactDataById(id);
-		if(contactData != null) {
+		if (contactData != null) {
 			deleteAddressBookContactData(id);
 			contactData.setFullName(addressBookDTO.fullName);
 			contactData.setAddress(addressBookDTO.address);
@@ -44,7 +51,7 @@ public class AddressBookService implements IAddressBookService {
 			contactData.setZip(addressBookDTO.zip);
 			contactData.setPhoneNumber(addressBookDTO.phoneNumber);
 			contactData = addressBookRepository.save(contactData);
-			}
+		}
 		return contactData;
 	}
 
